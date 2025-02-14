653d8ad61dea114945bb2d5927fe36fe0a1c696e
Hamza Usman <usmanrose338@gmail.com>
2025-02-14 07:07:36 +0100

    Updated project with latest changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+
 
 // // Function to generate tagname
 // const  generateTagname = async (username) => {
